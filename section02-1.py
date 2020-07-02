95917f64dc92eefb85d494a17fcae0354b583bb4
PAPION93 <sjun1319@gmail.com>
2020-07-02 23:57:49 +0900

    start python

---
+++ section02-1.py
@@ -0,0 +1,23 @@
+# Print() Play
+
+# Separator
+print('T', 'E', sep='')
+print('2020', '07', '02', sep='-')
+print('niceman', 'google.com', sep='@')
+
+# end
+print('Welcome To', end=' ')
+print('the black parade', end=' ')
+print('piano notes')
+
+# format [], {}, ()
+print('{} and {}' .format('You', 'Me'))
+print('{0} and {1} and {0}' .format('You', 'Me'))
+print('{a} and {b}' .format(a='You', b='Me'))
+
+# %s, %d, %f
+print("%s is string, %d is int, %f is float" % ("jun", 123, 0.1123))
+
+print("Test1: {0: 5d}, Price{1: 4.2f}". format(123, 1234.123))
+print("Test1: {a: 5d}, Price{b: 4.2f}". format(a=123, b=1234.123))
+
