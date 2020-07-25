48922ef7fce869db5d759716e503510dbe6b35be
PAPION93 <sjun1319@gmail.com>
2020-07-25 20:22:58 +0900

    로그인, 로그아웃 실습

--- 03_django_basic/community/user/views.py
+++ 03_django_basic/community/user/views.py
@@ -1,8 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import make_password, check_password
 from .models import UserCustom
 # Create your views here.
+
+
+def home(request):
+    user_id = request.session.get('user')
+
+    if user_id:
+        user = UserCustom.objects.get(pk=user_id)
+        return HttpResponse(user.username)
+
+    return HttpResponse("Home!")
+
+
+def logout(request):
+    if redirect.session.get('user'):
+        del(request.session['user'])
+
+    return redirect('/')
+
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+
+        res_data = {}
+        if not (username and password):
+            res_data['error'] = "모든 값을 입력해야합니다."
+        else:
+            user = UserCustom.objects.get(username=username)
+            if check_password(password, user.password):
+                request.session['user'] = user.id
+                return redirect('/')
+            else:
+                res_data['error'] = "비밀번호가 틀렸습니다."
+
+        return render(request, 'login.html', res_data)
 
 
 def register(request):
