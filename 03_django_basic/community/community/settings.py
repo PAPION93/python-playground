d2003569bf87f52a27cf0c3340967d5217ea8bc0
PAPION93 <sjun1319@gmail.com>
2020-08-02 23:42:53 +0900

    deploy 실습

--- 03_django_basic/community/community/settings.py
+++ 03_django_basic/community/community/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(n@u68kh)-5f0&7+vdbi-u87%9^z=h)o&5)sl(tmnc)n&+m-sf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    # 'papion.pythonanywhere.com'
+    '*'
+]
 
 
 # Application definition
@@ -121,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
