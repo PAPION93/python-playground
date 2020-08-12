775b137b65c5fa65135d4f6846b5ddff28a4371d
PAPION93 <sjun1319@gmail.com>
2020-08-12 23:36:25 +0900

    cv2.imshow mac bug..

--- 05_opencv/ch01/HelloCV.py
+++ 05_opencv/ch01/HelloCV.py
@@ -9,7 +9,10 @@ if img is None:
     print('Image load failed!')
     sys.exit()
 
-cv2.namedWindow('image')
+cv2.imwrite('./05_opencv/ch01/cat_gray.png', img)
+
+# cv2.namedWindow('image')
 cv2.imshow('image', img)
-cv2.waitKey(10)
-cv2.destroyAllWindows()
+cv2.waitKey()
+
+cv2.destroyAllWindows('image')
